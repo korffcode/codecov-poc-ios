c78f15e83b1b170478f1e58ef19539e97f61dbde
Adam Haafiz <adam.haafiz@warnermedia.com>
2023-06-26 16:13:11 +0200

    Add code without tests, expecting PR to be blocked
    Fix broken test
    
    Adjust thresholds
    
    Adjust thresholds again
    
    Disable tests
    
    v2 of PR fail test

--- Core/Sources/Core/Repositories/SearchHistoryRepository.swift
+++ Core/Sources/Core/Repositories/SearchHistoryRepository.swift
@@ -3,6 +3,7 @@ import Foundation
 public protocol SearchHistoryRepositoryProtocol {
     var queries: [String] { get }
     func save(_ query: String)
+    func superSave()
 }
 
 public class SearchHistoryRepository: SearchHistoryRepositoryProtocol {
@@ -16,5 +17,13 @@ public class SearchHistoryRepository: SearchHistoryRepositoryProtocol {
     
     public func save(_ query: String) {
         _queries.insert(query, at: 0)
+    }
+
+    public func superSave() {
+        _queries.insert("Super man!", at: 0)
+        _queries.insert("Super man!", at: 0)
+        _queries.insert("Super man!", at: 0)
+        _queries.insert("Super man!", at: 0)
+        _queries.insert("Super man!", at: 0)
     }
 }
